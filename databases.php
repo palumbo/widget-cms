94cf543ca925af17ee8c2b1b1f8974330beeda9f
Joseph Palumbo <jhpalumbo@gmail.com>
2016-01-04 23:43:08 -0500

    Adds databases.php with connection and disconnection to db

---
+++ databases.php
@@ -0,0 +1,30 @@
+<?php // we're going to connect to the database before the html 
+	
+	// 1. create the database connection 
+	$dbhost = "localhost"; 
+	$dbuser = "widget_cms"; 
+	$dbpass = "foo.bar"; 
+	$dbname = "widget_corp"; 
+	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); 
+	
+	if (mysqli_connect_errno()) { 
+		die("Database Connection Failed: " . 
+		mysqli_connect_error() . 
+		" -->" . mysqli_connect_errno() . "<--"
+		); 
+	}
+?>
+<html>
+	<head>
+		<title></title>
+	</head>
+	
+	<body>
+		
+		<script src=""></script>
+	</body>
+</html>
+
+<?php // 5. close the database connection
+	mysqli_close($connection); 
+?>
